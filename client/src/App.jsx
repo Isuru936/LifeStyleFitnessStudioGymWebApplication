cc6b67b22c6d2316cf700a3cd4cfdcce31d95344
Vinuka <vinuka.oveen@gmail.com>
2024-02-28 21:45:42 +0530

    test branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 // App.js
 import React from 'react';
-import WorkoutPool from './components/workoutpool';
+import WorkoutPool from './pages/workoutpool';
+import SideBar from './components/SideBar';
+//import CopyrightBar from './components/copyrightbar';
 
 function App() {
-  return (<div></div>
+  return (
+    <div>
+      <WorkoutPool/>
+      {/* <SideBar /> */}
+      {/*<CopyrightBar/>*/}
+    </div>
   );
 }
 
