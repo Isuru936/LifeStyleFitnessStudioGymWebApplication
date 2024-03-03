deeee02c7098c35a0ba3320dfc745faf9e734c8d
Bob <thamashaperera21@gmail.com>
2024-03-03 08:52:16 +0530

    fuck

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DietPlan from "./pages/DietPlanPool";
-import Home from "./pages/Home";
-import AssignDietPlan from "./pages/AssignDietPlan";
-import SideBar from "./components/SideBar";
-import SIDENAVBAR from "./components/SIDENAVBAR";
+import SamplePage from "./pages/SamplePage.jsx"
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/diet-plan" element={<DietPlan />} />
-          <Route path="/assign-diet-plan" element={<AssignDietPlan />} />
-          <Route path="/sidebar" element={<SideBar />} />
-          <Route path="/navBar" element={<SIDENAVBAR />} />
-        </Routes>
-      </BrowserRouter>
+      <SamplePage />
     </div>
   );
 }
