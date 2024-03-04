2985b78c331b58f16eab675f2e9161e225d89ebd
Bob <thamashaperera21@gmail.com>
2024-03-04 21:54:44 +0530

    Customer Feedback and User Management admin side COOMPLETED

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import UserDetails from "./pages/ViewUpdateUsers";
+import UserPool from "./pages/UserPool";
+import ContactUs from "./pages/ContactUs";
+import CustomerFeedbacks from "./pages/CustomerFeedbacks";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<UserDetails />} />
+          <Route path="/user-pool" element={<UserPool />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/customer-feedbacks" element={<CustomerFeedbacks />} />
         </Routes>
       </BrowserRouter>
     </div>
