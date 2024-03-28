6e7116281c97f26f47811efb0a84e9b7045f91f1
MohamedShafry <60726910+MohamedShafry@users.noreply.github.com>
2024-03-28 19:23:45 +0530

    Replaced All Q with a Single Quiz and all Q pages has been deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,9 @@ import Login  from './pages/Login';
 import Signup  from './pages/Signup';
 import ForgetPassword  from './pages/ForgetPassword';
 import NewPassword from './pages/NewPassword';
-import Q1 from './pages/Q1';
-import Q2 from './pages/Q2';
-import Q3 from './pages/Q3';
-import Q4 from './pages/Q4';
-import Q5 from './pages/Q5';
-import Q6 from './pages/Q6';
-import Q7 from './pages/Q7';
-import Q8 from './pages/Q8';
-import Q9 from './pages/Q9';
-import Q10 from './pages/Q10';
 import UserProfile from './pages/UserProfile';
 import EditProfile from './pages/EditProfile';
+import Quiz from './pages/Quiz';
 function App() {
 
   return (
@@ -24,16 +15,7 @@ function App() {
           <Route path='sign-up' element={<Signup/>}></Route>
           <Route path='forget-password' element={<ForgetPassword/>}></Route>
           <Route path='new-password' element={<NewPassword/>}></Route>
-          <Route path='Q1'element={<Q1/>}></Route>
-          <Route path='Q2'element={<Q2/>}></Route>
-          <Route path='Q3'element={<Q3/>}></Route>
-          <Route path='Q4'element={<Q4/>}></Route>
-          <Route path='Q5'element={<Q5/>}></Route>
-          <Route path='Q6'element={<Q6/>}></Route>
-          <Route path='Q7'element={<Q7/>}></Route>
-          <Route path='Q8'element={<Q8/>}></Route>
-          <Route path='Q9'element={<Q9/>}></Route>
-          <Route path='Q10'element={<Q10/>}></Route>
+          <Route path='Quiz'element={<Quiz/>}></Route>
           <Route path='profile' element={<UserProfile/>}></Route>
           <Route path='editProfile' element={<EditProfile/>}></Route>
         </Routes>
