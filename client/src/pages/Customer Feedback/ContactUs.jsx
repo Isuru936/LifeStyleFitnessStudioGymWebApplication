155db1e93d6e0aa5cf460e87624b24b49a258784
Bandara K. M. E <117734215+Isuru936@users.noreply.github.com>
2024-03-30 11:20:04 +0530

    Commited from Shafry

---
+++ client/src/pages/Customer Feedback/ContactUs.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import bgImg from "../../assets/about-us-bgImg.jpeg";
+import UserNavbar from "../../components/UserNavbar";
+
+function ContactUs() {
+  return (
+    <div>
+      <UserNavbar />
+      <div
+        className="inline-flex flex-col items-start min-h-screen w-screen bg-cover bg-center bg-no-repeat bg-fixed "
+        style={{
+          backgroundImage: `url(${bgImg})`,
+          backgroundSize: "auto",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "left",
+        }}
+      >
+        <div className="flex flex-col lg:flex-row m-2 lg:m-16 bg-slate-50 bg-opacity-40 rounded-2xl">
+          <div className=" mr-3 w-96 p-9  flex-1">
+            <h1 className="text-3xl font-bold mb-3">Write to Us!</h1>
+            <p className="font-semibold text-lg">Name</p>
+            <input
+              type="text"
+              placeholder="ex: Saman Bandara"
+              className="p-3 border w-full rounded-xl  outline-none"
+            />
+            <p>E-mail</p>
+            <input
+              type="email"
+              placeholder="ex: youremail@gmail.coms"
+              className="p-3 border w-full rounded-xl  outline-none"
+            />
+            <p>Message</p>
+            <textarea
+              name=""
+              id=""
+              cols="80"
+              rows="10"
+              className="border rounded-xl w-full outline-none p-5"
+              placeholder="Type your message here..."
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-orange-600 p-3 pl-5 pr-5 rounded-lg text-white font-bold hover:bg-orange-500 duration-300 transition-colors"
+            >
+              Send
+            </button>
+          </div>
+          <div className="flex-1 ml-3 p-1 lg:p-9">
+            <h1 className="text-3xl font-bold mb-3 ">Contact Us</h1>
+            <div className="mt-10 ml-5 ">
+              <div className="flex flex-row mb-5 hover:scale-110 translate duration-300 hover:text-orange-700">
+                <span
+                  className="icon-[logos--whatsapp-icon]"
+                  style={{ width: "36px", height: "36px" }}
+                />
+                <p className="font-bold pt-1 ml-5">+94 77 123 4567</p>
+              </div>
+              <div className=" mb-5 hover:scale-110 translate duration-300 hover:text-orange-700">
+                <a
+                  href="https://www.instagram.com/lifestyle_fitnessstudio/"
+                  className="flex flex-row"
+                  target="_blank"
+                >
+                  <span
+                    className="icon-[skill-icons--instagram] "
+                    style={{ width: "36px", height: "36px" }}
+                  />
+                  <p className="font-bold pt-1 ml-5 ">
+                    lifestyle_fitnessstudio
+                  </p>
+                </a>
+              </div>
+              <div className=" mb-5 hover:scale-110 translate duration-300 hover:text-orange-700">
+                <a
+                  href="https://www.youtube.com/channel/UCcVD0uHzvwMFE1KuQ9R9EEQ"
+                  className="flex flex-row"
+                  target="_blank"
+                >
+                  <span
+                    className="icon-[logos--youtube-icon] "
+                    style={{ width: "36px", height: "36px" }}
+                  />
+                  <p className="font-bold pt-1 ml-5 ">
+                    @lifestylefitnessstudio8155
+                  </p>
+                </a>
+              </div>
+              <div className=" mb-5 hover:scale-110 translate duration-300 hover:text-orange-700">
+                <a
+                  href="https://www.google.com/maps/place/LifeStyle+Fitness+Studio/@7.2876168,80.6153886,16.35z/data=!4m6!3m5!1s0x3ae368852367cca1:0xdb673bd4769ce11e!8m2!3d7.2866936!4d80.6181334!16s%2Fg%2F1q5bvbn43?entry=ttu"
+                  className="flex flex-row"
+                  target="_blank"
+                >
+                  <span
+                    className="icon-[openmoji--location-indicator-red]"
+                    style={{ width: "42px", height: "42px" }}
+                  />
+                  <p className="font-bold pt-1 ml-5 ">
+                    No.198/11/12 Lifestyle Fitness Studio George E de Silva
+                    Mawatha, Santa Maria Church Lane, Kandy 20000
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs;
---
+++ client/src/pages/Customer Feedback/CustomerFeedbacks.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import bgImg from "../../assets/asdad.jpeg";
+import DropDownNavBar from "../../components/DropDownNavBar";
+import SideBar from "../../components/SideBar";
+import logo from "../../assets/Logo.png";
+import { useState } from "react";
+
+function CustomerFeedbacks() {
+  const [mobileView] = useState(window.innerWidth < 768);
+  return (
+    <div>
+      <div
+        className="inline-flex flex-col items-start min-h-screen w-screen bg-cover bg-opacity-95 bg-center bg-no-repeat bg-fixed "
+        style={{
+          backgroundImage: `url(${bgImg})`,
+          backgroundSize: "contain",
+          backgroundPosition: "bottom",
+        }}
+      >
+        <div className="flex flex-col w-full ">
+          <div className="flex flex-row">
+            {/* side bar, select user */}
+            <div className="flex-col">
+              {mobileView ? <DropDownNavBar /> : <SideBar />}
+            </div>
+            <div className=" font-bold mt-0 ml-3 w-full">
+              <div className="flex bg-slate-50 bg-opacity-30 pt-5 text-4xl justify-between items-center">
+                Customer Feedbacks
+                <img src={logo} className="w-24 h-12 mr-5" alt="" />
+              </div>
+              <hr />
+              {/* table */}
+              <div className="flex flex-col w-full m-0">
+                <div className="overflow-x-auto">
+                  <div className="py-2 align-middle min-w-fit justify-center flex flex-col items-center">
+                    <div className="bg-slate-100 rounded-xl pl-3 mb-5">
+                      <span
+                        className="icon-[line-md--person-search-filled] "
+                        style={{ width: "24px", height: "24px" }}
+                      />
+                      <input
+                        type="text"
+                        placeholder="Search Customer Feedbacks"
+                        className="bg-slate-100 rounded-xl outline-none p-4"
+                      />
+                    </div>
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                      <table className="divide-y divide-gray-200 w-[800px]">
+                        <tbody className="bg-white divide-y divide-gray-200">
+                          <tr className="hover:bg-slate-200">
+                            <td className="px-6 py-4 whitespace-nowrap text-sm relative">
+                              <div className="flex justify-between">
+                                <p>Isuru Gayantha</p>
+                                <p className="justify-end absolute right-5 font-normal">
+                                  5 sec ago..
+                                </p>
+                              </div>
+                              <p className="font-normal">Yo Rock</p>
+                            </td>
+                          </tr>
+                          <tr className="hover:bg-slate-200">
+                            <td className="px-6 py-4 whitespace-nowrap text-sm relative">
+                              <div className="flex justify-between">
+                                <p>Isuru Gayantha</p>
+                                <p className="justify-end absolute right-5 font-normal">
+                                  5 days ago..
+                                </p>
+                              </div>
+                              <p className="font-normal whitespace-normal">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Animi atque praesentium, modi
+                                eveniet rerum architecto nulla quod vel maiores
+                                optio accusantium tempora soluta, magni et? Qui
+                                nesciunt maxime dolores ea.Lorem ipsum dolor sit
+                                amet consectetur adipisicing elit. Animi atque
+                                praesentium, modi eveniet rerum architecto nulla
+                                quod vel maiores optio accusantium tempora
+                                soluta, magni et? Qui nesciunt maxime dolores
+                                ea.
+                              </p>
+                            </td>
+                          </tr>
+                          <tr className="hover:bg-slate-100"></tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CustomerFeedbacks;
