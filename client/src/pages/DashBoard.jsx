f83e8be93aa48cad8d1981ffe1c3962a40f6b429
Bandara K. M. E <117734215+Isuru936@users.noreply.github.com>
2024-05-07 14:45:32 +0530

    resolving dependencies

---
+++ client/src/pages/DashBoard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "../components/UserNavbar.jsx";
+import video from "../assets/Snapinsta.app_video_DE4B219AB0DF57D567E63567642C3AAA_video_dashinit - Trim.gif";
+import bgImg from "../assets/435224b8d6c20d7126bc9eb88fa745dd.png";
+import BgVideo from "../components/BgVideo.jsx";
+
+function DashBoard() {
+  return (
+    <div>
+      <NavBar />
+      <BgVideo />
+    </div>
+  );
+}
+
+export default DashBoard;
