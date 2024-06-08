17d3a891ad51001394ec477619095226a05be353
Bandara K. M. E <117734215+Isuru936@users.noreply.github.com>
2024-06-08 14:52:26 +0530

    updated Cors

--- API/index.js
+++ API/index.js
@@ -33,7 +33,7 @@ app.use(CookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://lsfs-gym.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type"],
   })
